a6e842c50ebb5f0f8502562d786bcc64a262b350
Martin RA <marpht23@gmail.com>
2020-08-23 20:52:45 +0200

    Some wrong app links

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rsync -r src/* marand@192.168.87.177:/var/www/html
+rsync -vr src/* marand@192.168.87.177:/var/www/html
 #ssh marand@192.168.87.177 "chown -R www-data:www-data /var/www/html"
 
