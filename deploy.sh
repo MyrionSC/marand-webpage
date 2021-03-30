83058117db5346078f74ac3a08914c288493a80e
Martin RA <marpht23@gmail.com>
2021-03-30 18:29:05 +0200

    update deploy, remove typecraft, redrain

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-rsync -vr src/* marand@192.168.87.177:/var/www/html
+rsync -vr src/* marand@marand.dk:/var/www/marand
 #ssh marand@192.168.87.177 "chown -R www-data:www-data /var/www/html"
 
